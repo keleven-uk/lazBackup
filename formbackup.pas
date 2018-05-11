a05e45b9158fde12bf58b0f2e8b74d4f1b3d2759
keleven-uk <github@keleven.co.uk>
2018-05-11 22:47:02 +0100

    The display now consists of three list boxes, one for the source directory, one for the destination director and the middle one for the differences.
    Added logic to walk the source and destination directories, after both have
    been specified.
    
    Added the Analyse function which determined what files have been added to the
    source and not in destination, have been deleted from the destination i.e. no
    longer in source.

--- formbackup.pas
+++ formbackup.pas
@@ -6,24 +6,48 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  ComCtrls, Menus, formAbout, formhelp, formOptions, formLicence, uOptions;
+  ComCtrls, Menus, EditBtn, StdCtrls, formAbout, formhelp, formOptions,
+  formLicence, uOptions, windows;
 
 type
 
   { TfrmMain }
 
   TfrmMain = class(TForm)
-    mnuLicence   : TMenuItem;
-    mnuItmOptions: TMenuItem;
-    mnuItmHelp   : TMenuItem;
-    mnuItmAbout  : TMenuItem;
-    mnuItmExit   : TMenuItem;
-    mnuhelp      : TMenuItem;
-    mnuFile      : TMenuItem;
-    mnuMain      : TMainMenu;
-    stsBrInfo    : TStatusBar;
-    Timer1       : TTimer;
+    lblDeleteFiles: TLabel;
+    Panel1              : TPanel;
+    Bevel1              : TBevel;
+    Bevel2              : TBevel;
+    Bevel3              : TBevel;
+    Bevel4              : TBevel;
+    btnAnalyse          : TButton;
+    btnCopy             : TButton;
+    drctryEdtStub       : TDirectoryEdit;
+    drctryEdtSource     : TDirectoryEdit;
+    DrctryEdtDestination: TDirectoryEdit;
+    Label1              : TLabel;
+    Label2              : TLabel;
+    lblDestination      : TLabel;
+    lblSource           : TLabel;
+    lblNewFiles         : TLabel;
+    lstBxSource         : TListBox;
+    lstBxDifferance     : TListBox;
+    lstBxDestination    : TListBox;
+    mnuLicence          : TMenuItem;
+    mnuItmOptions       : TMenuItem;
+    mnuItmHelp          : TMenuItem;
+    mnuItmAbout         : TMenuItem;
+    mnuItmExit          : TMenuItem;
+    mnuhelp             : TMenuItem;
+    mnuFile             : TMenuItem;
+    mnuMain             : TMainMenu;
+    PrgrssBrAnalyse     : TProgressBar;
+    stsBrInfo           : TStatusBar;
+    Timer1              : TTimer;
 
+    procedure btnAnalyseClick(Sender: TObject);
+    procedure DrctryEdtDestinationChange(Sender: TObject);
+    procedure drctryEdtSourceChange(Sender: TObject);
     procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
     procedure FormCreate(Sender: TObject);
     procedure mnuItmAboutClick(Sender: TObject);
@@ -34,6 +58,13 @@ type
     procedure Timer1Timer(Sender: TObject);
   private
     { private declarations }
+    procedure walkDirectory(dir: string; mode: string);
+    procedure destinationFileFound(FileIterator: TFileIterator);
+    procedure sourceFileFound(FileIterator: TFileIterator);
+    procedure directoryFound(FileIterator: TFileIterator);
+    procedure FindSourceFiles();
+    procedure FindDestinationFiles();
+    function fileSizeToHumanReadableString(fileSize: Int64): string;
   public
     noOfTicks : integer ;
   end; 
@@ -44,6 +75,20 @@ var
   debugFle    : text;
   debug       : Boolean;
   appStartTime: int64;          //  used by formAbout to determine how long the app has been running.
+  filesSize   : int64;          //  used to hold the total size of all files.
+  noOfFiles   : longint;        //  used to hold the total number of files.
+  noOfDirs    : longint;        //  used to hold the total number of directories
+  source      : boolean;        //  true if source files been walked.
+  destination : boolean;        //  true if destination files have been walked.
+
+const
+  OneKB = Int64(1024);          //  constants used in TfrmMain.FileSizeToHumanReadableString
+  OneMB = Int64(1024) * OneKB;
+  OneGB = Int64(1024) * OneMB;
+  OneTB = Int64(1024) * OneGB;
+  OnePB = Int64(1024) * OneTB;
+  fmt = '#.###';
+
 implementation
 
 {$R *.lfm}
@@ -59,7 +104,7 @@ begin
   debug := true ;
 
   if debug then begin
-    DebugFleName := 'lazbaCKUP.log';
+    DebugFleName := 'lazBackup.log';
     assignfile(debugFle, DebugFleName);
     rewrite(debugFle);
     writeLn(debugFle, format ('%s : %s Created', [timeToStr(now), DebugFleName]));
@@ -67,8 +112,11 @@ begin
 
   userOptions := Options.Create;  // create options file as c:\Users\<user>\AppData\Local\lazbackup\Options.xml
 
-  frmMain.Top := UserOptions.formTop;
+  frmMain.Top  := UserOptions.formTop;
   frmmain.Left := UserOptions.formLeft;
+
+  source      := false;
+  destination := false;
 end;
 
 
@@ -84,7 +132,21 @@ begin
 
   userOptions.writeCurrentOptions;  // write out options file.
 end;
+//
+// ****************************** Directory Edits ******************************
+//
+procedure TfrmMain.drctryEdtSourceChange(Sender: TObject);
+begin
+  walkDirectory(drctryEdtSource.Directory, 'S');
+end;
 
+procedure TfrmMain.DrctryEdtDestinationChange(Sender: TObject);
+begin
+  walkDirectory(drctryEdtDestination.Directory, 'D');
+end;
+//
+// ****************************** Menu Items ***********************************
+//
 procedure TfrmMain.mnuItmAboutClick(Sender: TObject);
 begin
   frmAbout.ShowModal;
@@ -109,11 +171,205 @@ procedure TfrmMain.mnuLicenceClick(Sender: TObject);
 begin
   frmLicence.Show;
 end;
-
+//
+// ****************************** Timer ****************************************
+//
 procedure TfrmMain.Timer1Timer(Sender: TObject);
 begin
   stsBrInfo.Panels.Items[0].Text := TimeToStr(Time) ;
   stsBrInfo.Panels.Items[1].Text := FormatDateTime('DD MMM YYYY', Now);
+end;
+//
+// ****************************** Analyse **************************************
+//
+procedure TfrmMain.btnAnalyseClick(Sender: TObject);
+
+begin
+  btnAnalyse.Enabled := false;
+
+  FindSourceFiles;
+  FindDestinationFiles;
+
+  PrgrssBrAnalyse.Position := 0;
+  Label2.Enabled           := true;
+end;
+
+procedure TfrmMain.FindSourceFiles();
+VAR
+  f       : integer;
+  srcFile : string;
+  indpos  : integer;
+  newFiles: integer;
+begin
+  newFiles := 0;
+
+  PrgrssBrAnalyse.min      := 0;
+  PrgrssBrAnalyse.Max      := lstBxSource.Count;
+  PrgrssBrAnalyse.Position := 0;
+
+  for f := 0 to lstBxSource.Count -1 do
+  begin
+    // process all user events, like clicking on the button
+    Application.ProcessMessages;
+    if Application.Terminated then close;  //  exit clicked
+
+
+    srcFile := drctryEdtStub.Directory.ToLower + '\' + lstBxSource.Items.Strings[f].Replace(':', '');
+    indpos  := lstBxDestination.Items.IndexOf(srcFile);
+
+    if indpos = -1 then
+    begin
+      lstBxDifferance.Items.Add('NEW : ' + lstBxSource.Items.Strings[f]);
+      newFiles += 1;
+      lblNewFiles.Caption := format('%d NEW files', [newFiles]);;
+    end;  //  if indpos = -1 then
+
+    PrgrssBrAnalyse.Position := PrgrssBrAnalyse.Position + 1;
+  end;    //  for f := 0 to lstBxSource.Count -1 do
+
+end;
+
+procedure TfrmMain.FindDestinationFiles();
+VAR
+  f       : integer;
+  dstFile : string;
+  indpos  : integer;
+  delFiles: integer;
+  dstlen  : integer;
+begin
+  delFiles := 0;
+
+  PrgrssBrAnalyse.min      := 0;
+  PrgrssBrAnalyse.Max      := lstBxDestination.Count;
+  PrgrssBrAnalyse.Position := 0;
+
+  for f := 0 to lstBxDestination.Count -1 do
+  begin
+    // process all user events, like clicking on the button
+    Application.ProcessMessages;
+    if Application.Terminated then close;  //  exit clicked
+
+    dstFile := lstBxDestination.Items.Strings[f];
+    dstlen  :=  length(drctryEdtStub.Directory.ToLower ) + 1;
+    delete(dstFile, 1, dstlen);                          //  Must remember, string start at position 1.
+    Insert(':', dstFile, 2);                             //  Insert the : we removed earlier.
+    indpos := lstBxsource.Items.IndexOf(dstFile);
+
+    if indpos = -1 then
+    begin
+      lstBxDifferance.Items.Add('DELETE : ' + dstFile);
+      delFiles += 1;
+      lblDeleteFiles.Caption := format('%d DELETE files', [delFiles]);;
+    end;  //  if indpos = -1 then
+
+    PrgrssBrAnalyse.Position := PrgrssBrAnalyse.Position + 1;
+  end;    //  for f := 0 to lstBxDestination.Count -1 do
+
+end;
+//
+// ****************************** Walk Directory *******************************
+//
+procedure TfrmMain.walkDirectory(dir: string; mode: string);
+{  Walk a directory passed into procedure.
+   Does not check if directory exists.
+}
+VAR
+  fileSearch  : TFileSearcher;
+begin
+  filesSize := 0;
+  noOfFiles := 0;
+  noOfDirs  := 0;
+
+  //  actual search
+  fileSearch := TFileSearcher.Create;
+  if mode = 'S' then
+    fileSearch.OnFileFound := @sourceFileFound
+  else
+    fileSearch.OnFileFound := @destinationFileFound;
+  fileSearch.OnDirectoryFound := @directoryFound;
+  fileSearch.Search(dir, '*.*', True);
+  fileSearch.Free;
+  //  search finished.
+
+  if mode = 'S' then
+    source := true                //  Source has been walked.
+  else
+    destination := true;          //  Destination has been walked.
+
+  if source and destination then  //  Only enable Analyse button if both
+    begin                         //  source and destination have been walked.
+      btnAnalyse.Enabled := true;
+      Label1.Enabled     := true;
+
+      drctryEdtSource.Enabled      := false;
+      DrctryEdtDestination.Enabled := false;
+      drctryEdtStub.Enabled        := false;
+    end;
+
+end;
+
+procedure TfrmMain.sourceFileFound(FileIterator: TFileIterator);
+begin
+  // process all user events, like clicking on the button
+  Application.ProcessMessages;
+  if Application.Terminated then close;  //  exit clicked
+
+  noOfFiles += 1;
+  filesSize := filesSize + FileIterator.FileInfo.Size ;
+
+  lstBxSource.Items.Add(FileIterator.FileName.ToLower);
+
+  lblSource.Caption := format('%d files [%s bytes]   %d directories',
+               [noOfFiles, FileSizeToHumanReadableString(filesSize), noOfDirs]);
+end;
+
+procedure TfrmMain.destinationFileFound(FileIterator: TFileIterator);
+begin
+  // process all user events, like clicking on the button
+  Application.ProcessMessages;
+  if Application.Terminated then close;  //  exit clicked
+
+  noOfFiles += 1;
+  filesSize := filesSize + FileIterator.FileInfo.Size ;
+
+  lstBxDestination.Items.Add(FileIterator.FileName.ToLower);
+
+  lblDestination.Caption := format('%d files [%s bytes]   %d directories',
+               [noOfFiles, FileSizeToHumanReadableString(filesSize), noOfDirs]);
+end;
+
+procedure TfrmMain.directoryFound(FileIterator: TFileIterator);
+begin
+  noOfDirs += 1;
+end;
+
+function TfrmMain.fileSizeToHumanReadableString(fileSize: Int64): string;
+{  Returns filesize in a human readable form.
+   Does not use ther silly ISO standard unit of Pib, TiB, GiB, MiB & KiB.
+   Used the gold old fashion units of Pib, TB, GB, MB & KB.
+}
+
+begin
+  if fileSize > OnePB then
+    result := FormatFloat(fmt + 'PB', fileSize / OnePB)
+  else
+    if fileSize > OneTB then
+      result := FormatFloat(fmt + 'TB', fileSize / OneTB)
+    else
+      if fileSize > OneGB then
+        result := FormatFloat(fmt + 'GB', fileSize / OneGB)
+      else
+        if fileSize > OneMB then
+          result := FormatFloat(fmt + 'MB', fileSize / OneMB)
+        else
+          if fileSize > OneKB then
+            result := FormatFloat(fmt + 'KB', fileSize / OneKB)
+          else
+            if fileSize > 0 then
+              result := FormatFloat(fmt + 'bytes', fileSize)
+            else
+              result := ''
+
 end;
 
 end.
